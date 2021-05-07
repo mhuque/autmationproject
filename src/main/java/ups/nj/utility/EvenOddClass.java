ebf5c363071e2db66376e56e7a41828ad7318082
mhuque07@gmail.com <mhuque07@gmail.com>
2021-05-07 19:33:33 -0400

    third commit by mir

---
+++ src/main/java/ups/nj/utility/EvenOddClass.java
@@ -0,0 +1,19 @@
+package ups.nj.utility;
+
+public class EvenOddClass {
+	
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+		int a = 11;
+		if (a%2 == 0) {
+			System.out.println("This is an even number : "+a);		
+		} else {
+			System.out.println("This is an odd number : "+a);		
+		}	
+	
+}
+}
